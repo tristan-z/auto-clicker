9890f3e5623df43f6a3197821022296f27d00405
Tristan Zickovich <tz200c@att.com>
2023-03-21 15:53:20 -0700

    Add Image Event Tests

--- src/tests/test_event_actions.py
+++ src/tests/test_event_actions.py
@@ -1,7 +1,9 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from event.constants import CLICK_TYPES
-from event.events import ClickEvent
+from event.events import ClickEvent, ImageEvent
+from event.exceptions import FindImageError
+from pyautogui import ImageNotFoundException
 
 
 class TestClickEvents(unittest.TestCase):
@@ -25,6 +27,38 @@ class TestClickEvents(unittest.TestCase):
 
         ClickEvent.perform_click(mock_coords, mock_offset, mock_click_type)
         mock_randint.assert_called_with(mock_offset * -1, mock_offset)
+
+
+class TestImageEvents(unittest.TestCase):
+    @patch("event.events.pyautogui.locateCenterOnScreen")
+    def test_find_image(self, mock_locate):
+        mock_image_path = "test_image.png"
+        mock_region = (100, 200, 300, 400)
+        mock_locate.return_value = (150, 150)
+        result = ImageEvent.find_image(
+            mock_image_path,
+            confidence=0.6,
+            grayscale=False,
+            region=mock_region,
+            attempts=1,
+        )
+        self.assertEqual(result, (150, 150))
+
+    @patch("event.events.Delay.perform_delay_ms")
+    @patch("event.events.pyautogui.locateCenterOnScreen")
+    def test_image_not_found(self, mock_locate, _):
+        mock_locate.raiseError.side_effect = ImageNotFoundException()
+        mock_image_path = "test_image.png"
+        mock_region = (100, 200, 300, 400)
+
+        with self.assertRaises(FindImageError):
+            ImageEvent.find_image(
+                mock_image_path,
+                confidence=0.6,
+                grayscale=False,
+                region=mock_region,
+                attempts=3,
+            )
 
 
 if __name__ == "__main__":
